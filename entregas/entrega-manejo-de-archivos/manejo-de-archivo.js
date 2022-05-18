e428e7da91e9cb1d2443897d38b60e15661461b3
francisco casañas <francasanas@gmail.com>
2022-05-18 20:13:51 -0300

    entrega-manejo-arc

--- entregas/entrega-manejo-de-archivos/manejo-de-archivo.js
+++ entregas/entrega-manejo-de-archivos/manejo-de-archivo.js
@@ -1,55 +1,91 @@
-const fs=require('fs')
+const fs = require('fs');
 
-const productos = [];
-class Archivo{
-    constructor(nombre){
-        this.nombre=nombre  
+const productos = [
+    {
+        id: 1,
+        nombre: "telefono",
+        precio: 1000,
+        thumbnail: "https://blog.movical.net/wp-content/uploads/2019/11/encontrar-el-archivo-boot.img-en-un-tel%C3%A9fono-Huawei-min-1024x1024.jpg"
+    },
+    {
+        id: 2,
+        nombre: "computadora",
+        precio: 3000,
+        thumbnail: "https://www.cetrogar.com.ar/media/catalog/product/c/m/cm3251.jpg?width=500&height=500&canvas=500:500&quality=80&bg-color=255,255,255&fit=bounds"
+    },
+    {
+        id: 3,
+        nombre: "tablet",
+        precio: 2000,
+        thumbnail: "https://www.musicalisimo.com.ar/public/images/productos/grande/producto_4279_1.jpg"
     }
+];
 
-    async leer(){
-        try{
-            let st= await fs.promises.readFile(this.nombre, 'utf-8')  
-            return st
-        }catch(err){
-            console.error([])
+fs.writeFileSync("./productos.txt", JSON.stringify(productos));
+let data = fs.readFileSync("./productos.txt", "utf-8");
+data = JSON.parse(data);
+
+class Contenedor {
+    constructor(productos) {
+        this.productos = productos
+    }
+    async save(productoNuevo) {
+        try {
+            this.productos.push(productoNuevo);
+            await fs.promises.writeFile("./productos.txt", JSON.stringify(this.productos), error => { if (error) { console.log(error); } else console.log(""); });
+
+            let arrayId = this.productos.map(e => e.id);
+            let idMax = Math.max(...arrayId);
+            return console.log(idMax);
+        }
+        catch (error) {
+            console.log(error);
         }
     }
-    async guardar(producto){
-        try{
-            productos.push(producto)
-            await fs.promises.writeFile(this.nombre, JSON.stringify(productos, null, '\t'))
-            return producto.id
-        }catch(err){
-            console.error('No se pudo guardar: ' + err)
+    getById(num) {
+        try {
+            let idFiltrado = this.productos.filter(e => num === e.id)
+            return console.log(idFiltrado);
+        } catch (error) {
+            console.log(error);
         }
     }
-    async borrar(){
-        try{
-            await fs.promises.unlink(this.nombre, (err)=>{
-                if(err) return console.log('Error archivo borrado: ' + err)
-            })
-        }catch(err){
-            console.error('Error archivo borrado: ' + err)
+    async getAll() {
+        try {
+            let dato = await fs.promises.readFile("./productos.txt", "utf-8");
+            console.log(JSON.parse(dato));
+        } catch (err) {
+            return []
+        }
+    }
+    async deleteById(num) {
+        try {
+            let arregloNuevo = [];
+            arregloNuevo.push(this.productos.filter(e => num !== e.id));
+            await fs.promises.writeFile("./productos.txt", JSON.stringify(arregloNuevo), error => { if (error) { console.log(error); } else console.log(""); });
+            return console.log(arregloNuevo);
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+    async deleteAll() {
+        try {
+            let arrayVacio = []
+            await fs.promises.writeFile("./productos.txt", arrayVacio, error => { if (error) { console.log(error); } else console.log(""); })
+            return console.log(arrayVacio);
+        } catch (err) {
+            console.log(err);
         }
     }
 }
-const archivo = new Archivo('./productos.txt')
-archivo.guardar({
-    title: 'remeras', 
-    price: 1000, 
-    thumbnail: 'https://i.imgur.com/yo2QIDw.jpg',
-    id:productos.length + 1
-})
-archivo.guardar({
-    title: 'anillo', 
-    price: 2000, 
-    thumbnail: 'https://i.imgur.com/tgYSPkj.jpg',
-    id:productos.length + 1
-})
-archivo.guardar({
-    title: 'pollera', 
-    price: 1500, 
-    thumbnail: 'https://i.imgur.com/m2LLMql.jpg',
-    id:productos.length + 1
-})
-archivo.leer()
+
+
+
+const nuevoContenedor = new Contenedor(data);
+
+nuevoContenedor.save({ id: 4, nombre: "pantalla", precio: 2000 });
+nuevoContenedor.getById(2);
+nuevoContenedor.getAll();
+nuevoContenedor.deleteById(1);
+nuevoContenedor.deleteAll();
