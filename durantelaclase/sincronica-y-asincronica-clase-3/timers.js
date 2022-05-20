8fc397e62bef2a2904089eec06a3811b7e9df6a8
francisco casañas <francasanas@gmail.com>
2022-05-20 13:51:22 -0300

    subiendocambios

---
+++ durantelaclase/sincronica-y-asincronica-clase-3/timers.js
@@ -0,0 +1,10 @@
+
+setTimeout(()=>{
+    let time = new Date()
+    console.log(time, 'Saludos a Francisco Casañas')
+}, 3000)
+setInterval(()=>{
+    let time = new Date()
+    console.log(time, 'Saludos  ricky')
+}, 2000)
+
